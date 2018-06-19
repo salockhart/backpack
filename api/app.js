e247e1af6b1410e6481ce43b49928b5b807182a3
salockhart <salexlockhart@gmail.com>
2018-06-19 19:38:49 -0300

    Commit submitted posts

--- api/app.js
+++ api/app.js
@@ -2,25 +2,14 @@ let fs = require('fs');
 let path = require('path');
 let express = require('express');
 let multer = require('multer');
-
-let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'assets/img/')
-    },
-    filename: function (req, file, cb) {
-        console.dir(file);
-        cb(null, fileToName(req, file));
-    }
-  })
-  
-let upload = multer({ storage: storage })
+let { exec } = require('child_process');
+let sanitize = require('sanitize-filename');
 
 const app = express();
 
 const port = (process.env.PORT || 3000);
 app.set('port', port);
 
-// app.use(express.json());
 app.use(express.urlencoded({
   extended: true,
 }));
@@ -30,6 +19,10 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
   next();
+});
+
+app.get('/', (req, res) => {
+    return res.send('Hello World');
 });
 
 let template = (author, image, content) => `\
@@ -42,43 +35,46 @@ image: ${image}
 ${content}\n\
 `;
 
-let dateToStamp = (date) => {
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1 + "";
+let getFileName = (date, title) => {
+    let dateObj = new Date(date);
+    let year = dateObj.getFullYear();
+    let month = dateObj.getMonth() + 1 + "";
     if (month.length < 2) {
         month = "0" + month;
     }
-    let day = date.getDate() + "";
+    let day = dateObj.getDate() + "";
     if (day.length < 2) {
         day = "0" + day;
     }
-
-    return {
-        year,
-        month,
-        day,
-    };
+    return sanitize(`${year}-${month}-${day}-${title}`.toLowerCase().replace(/ /g, '-'));
 };
 
-let fileToName = (req, file) => {
-    let date = dateToStamp(new Date(req.body.date));
-    return `${date.year}-${date.month}-${date.day}-${req.body.title}${path.extname(file.originalname)}`;
-};
-
-app.get('/', (req, res) => {
-    return res.send('Hello World');
-});
-
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'assets/img/')
+    },
+    filename: function (req, file, cb) {
+        console.dir(file);
+        cb(null, `${getFileName(req.body.date, req.body.title)}${path.extname(file.originalname)}`);
+    }
+})
+  
+let upload = multer({ storage: storage })
 app.post('/', upload.fields([{ name: 'image', maxCount: 1 }]), (req, res) => {
     console.dir(req.body);
     console.dir(req.files);
 
-    let date = dateToStamp(new Date(req.body.date));
+    let filename = getFileName(req.body.date, req.body.title);
+    if (filename.length === 0) {
+        res.sendStatus(400);
+    }
+    let path = `_posts/${filename}.md`;
+    let post = template(req.body.author, req.files.image[0].filename, req.body.content);
+    fs.writeFile(path, post);
 
-    let filename = `_posts/${date.year}-${date.month}-${date.day}-${req.body.title}.md`;
-    fs.writeFile(filename, template(req.body.author, req.files.image[0].filename, req.body.content));
-
-    res.sendFile(path.resolve(filename));
+    exec(`git add _posts assets/img && git commit -m "${filename}" && git push`, (err, stdout, stderr) => {
+        res.sendStatus(200);
+    });
 });
 
 app.listen(port, () => {
